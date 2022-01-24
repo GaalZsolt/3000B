c3dc91664eaa507e5bc8d4479b9fda6742b0b6c9
Gaál Zsolt <gaal.zsolt.19f@szlgbp.hu>
2022-01-24 11:12:17 +0100

    feladatleírások

--- elso.py
+++ elso.py
@@ -1,6 +1,7 @@
 # 1. Van-e a sorozatban pozitív szám?
 
 def feladat1(lista):
+    print("1. Van-e a sorozatban pozitív szám?")
     for a in lista:
         if 0 < int(a):
             return True
--- harmadik.py
+++ harmadik.py
@@ -4,6 +4,7 @@ from logging import exception
 
 
 def feladat3(lista) -> int:
+    print("3. Mennyi a sorozatban található legkisebb szám?")
     min = int(lista[0])
     for a in lista:
         if min < int(a):
