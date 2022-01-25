dd39d5ab69e05220a46f490d61b87a0edf65e4b0
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-01-25 10:53:56 +0100

    ÖTÖS MEGOLDVA!!!!!!!!!

--- otodik.py
+++ otodik.py
@@ -1,1 +1,9 @@
-print('otodik feladat')
+print('5. Mennyi a sorozatban található számok átlagának a fele?')
+
+def feladat5(lista):
+    sum = 0
+    for e in lista:
+        sum+=e
+    return (sum/len(lista))/2
+        
+
