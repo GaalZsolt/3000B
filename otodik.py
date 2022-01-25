4d3af3819038025af90572b4b66e1e3cd971d476
szabo bence <szabo.bence.19f@szlgbp.hu>
2022-01-25 10:57:03 +0100

    otötös kijavitva

--- otodik.py
+++ otodik.py
@@ -3,7 +3,7 @@ print('5. Mennyi a sorozatban található számok átlagának a fele?')
 def feladat5(lista):
     sum = 0
     for e in lista:
-        sum+=e
+        sum+=int(e)
     return (sum/len(lista))/2
         
 
